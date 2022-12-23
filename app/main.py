fa28c19129196ddc68e0e46aba2fda7d40465d4d
LamideK <119982368+LamideK@users.noreply.github.com>
2022-12-22 19:52:50 -0800

    Update: Introduction of ORM
    - sqlalchemy ORM is used to communicate with the db rather than using direct queries
    - database file added to manage all db initialization processes (not included yet)
    - db & db structure is now defined with models.py
    - external notes added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -8,12 +8,19 @@ import psycopg
 import psycopg_binary
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
 #from psycopg2.extras import RealDictCursor
 #from psycopg_binary.extras import RealDictCursor
 
-
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+
 
 class Post(BaseModel):
     title: str
@@ -25,7 +32,7 @@ class Post(BaseModel):
 #connect to the database // Also, try again with plain psycopg
 while True:
     try:
-        conn = psycopg2.connect(host= 'localhost', database= 'FASTAPI', user= 'postgres', password= 'User@123', cursor_factory= RealDictCursor)  #, cursor_factory= RealDictCursor
+        conn = psycopg2.connect(host= 'localhost', database= 'FASTAPI', user= 'postgres', password= 'passw0rd', cursor_factory= RealDictCursor)  #, cursor_factory= RealDictCursor
         cursor = conn.cursor()
         print('Successful')
         break
@@ -44,6 +51,13 @@ my_posts = [{"title": "title of post", "content": "content of post", "id":1}, {"
 async def root():
     return {"message": "Hello LamBam"}
 
+
+@app.get('/sqlalchemy')
+def test_posts(db: Session = Depends(get_db)):
+
+    posts = db.query(models.Post).all()
+    return{"data": posts}
+
 def find_post(id):
     for p in my_posts:
         if p['id'] == id: #comparing string and int won't work
@@ -57,18 +71,30 @@ def find_post_index(id):
 
 
 @app.get('/posts')
-async def get_posts():
+async def get_posts(db: Session= Depends(get_db)):
+    '''
+    USING DIRECT SQL QUERY
     cursor.execute("""Select * from posts""")
     posts = cursor.fetchall()
-    print(posts)
+    '''
+
+    posts = db.query(models.Post).all()
     return{"data": posts}
 
 
 @app.post('/newpost', status_code=status.HTTP_201_CREATED)
-async def create_posts(new_post: Post):
+async def create_posts(new_post: Post, db: Session= Depends(get_db)):
+    '''
     cursor.execute("""INSERT INTO posts(title, content, published) values (%s, %s, %s) RETURNING *""" , (new_post.title, new_post.content, new_post.published))
     post = cursor.fetchone()
     conn.commit()
+    '''
+
+    post = models.Post(title= new_post.title, content= new_post.content, published= new_post.published)
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    #post =db.query(models.Post)
     return{"data successfully added": post}
 
 
@@ -86,10 +112,11 @@ async def get_post(id: int):
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id:int):
     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
-    delete_post = cursor.fetchone()
+    deleted_post = cursor.fetchone()
     conn.commit()
 
-    if delete_post == None:
+# might tweak
+    if deleted_post == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
      #return{"message: ": "post successfully deleted"} #preferred
     return Response(status_code=status.HTTP_204_NO_CONTENT) #this is standard practice
@@ -97,7 +124,7 @@ async def delete_post(id:int):
 
 @app.put('/posts/{id}')
 async def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = RETURNING *""", (post.title, post.content, post.published, id))
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     updated_post = cursor.fetchone()
     conn.commit()
 
